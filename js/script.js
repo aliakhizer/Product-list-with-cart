9f289987a7a503d9e54d6a89df71daf17f5a029f
Haris Shah <harisshah385@gmail.com>
2024-09-09 00:22:17 +0500

    enhance javascript and CSS

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
-fetch("./data.json")
+const cardsContainer = document.querySelector('.products-card')
+const cartContainer = document.querySelector('.cart-container')
+
+let cartArray = []
+let orderCard = null
+let total = 0
+
+fetch('./data.json')
   .then((response) => {
-    return response.json(); // Corrected: Call the function with parentheses
+    return response.json()
   })
   .then((products) => {
-    let productArray = "";
-    let cartArray = " ";
-    let orderCard = " ";
-    let Count = 0;
-
-    products.forEach((product) => {
-      productArray += `
+    products.forEach((product, i) => {
+      let card = document.createElement('div')
+      card.innerHTML = `
      <div class="product-card">
             <img
               src=${product.image.desktop}
-              alt="waffle img"
+              alt="${product.name}"
               class="desert-img"
             />
-            <button class="cart-btn" data-product='${JSON.stringify(product)}'>
+            <button class="cart-btn" data-add-to-cart="${product.id}">
               <img src="./assets/images/icon-add-to-cart.svg" alt="" />
               add to cart
             </button>
@@ -25,70 +28,64 @@ fetch("./data.json")
               <div class="product-extra-flavour">${product.category}</div>
               <div class="product-price">${product.price}</div>
             </div>
-          
-       </div> `;
-    });
-    cartArray += `<div class="cart-title">
-            Your Cart(<span id="cart-count">0</span>)
-          </div>
-          <div class="cart-img">
-            <img src="./assets/images/illustration-empty-cart.svg" alt="" />
-          </div>
-          <div class="cart-des">Your added items will appear here</div>
-        </div>`;
+       </div> `
+      cardsContainer.appendChild(card)
 
-    document.querySelector(".products-card").innerHTML = productArray;
-    document.querySelector(".cart-container").innerHTML = cartArray;
+      const addToCart = card.querySelector(`[data-add-to-cart="${product.id}"]`)
 
-    document.querySelectorAll(".cart-btn").forEach((button) => {
-      button.addEventListener("click", function () {
-        Count++;
-        console.log(Count);
-        const productData = JSON.parse(this.getAttribute("data-product"));
-        console.log("productData", productData);
-
-        updateCard(productData, Count);
-      });
-    });
-
-    function updateCard(product, count) {
-      console.log("product", product);
-
-      const orderCard = `
-        <div class="order-title">Your Cart(${count})</div>
-        <div class="order-details">
-          <div class="order-flavours">
-            <h3 class="order-title-flvr">${product.name}</h3>
-            <div class="order-price-details">
-              <p class="order-size">1x</p>
-              <div class="order-price">
-                <p class="price">@$${product.price}</p>
-                <p class="order-total-price">$${product.price}</p>
-              </div>
-            </div>
-          </div>
-          <div class="order-cancel">
-            <img
-              src="./assets/images/icon-remove-item.svg"
-              alt=""
-              class="cancel"
-            />
-          </div>
-        </div>
-        <hr class="order-divider" />
-        <div class="total-amount">
-          <p class="total-amount-title">Order Total</p>
-          <div class="total-amount-price">$${product.price}</div>
-        </div>
-        
-        <div class="order-confirm">
-          <button class="confirm-btn">Confirm Order</button>
-        </div>
-      `;
-      document.querySelector(".cart-container").innerHTML = orderCard;
-    }
+      addToCart.addEventListener('click', () => {
+        // console.log(product)
+        // console.log(product.id === cartArray.includes(product.id))
+        // if (product.id !== cartArray.includes(product.id)) {
+        //   return
+        // }
+        cartArray.push(product)
+        getCartItems()
+      })
+    })
+  })
+  .catch((error) => {
+    console.log(error)
   })
 
-  .catch((error) => {
-    console.log("error");
-  });
+function getCartItems() {
+  if (cartArray.length !== 0) {
+    cartContainer.innerHTML = ''
+    cartArray.forEach((item) => {
+      let cartItem = document.createElement('div')
+      cartItem.innerHTML = `
+      <div class="order-container">
+      <div class="order-title">Your Cart(${cartArray.length})</div>
+      <div class="order-details">
+        <div class="order-flavours">
+          <h3 class="order-title-flvr">${item.name}</h3>
+          <div class="order-price-details">
+            <p class="order-size">1x</p>
+            <div class="order-price">
+              <p class="price">${item.price}</p>
+              <p class="order-total-price">$5.50</p>
+            </div>
+          </div>
+        </div>
+        <div class="order-cancel" data-remove-from-cart="${item.id}">
+          <img
+            src="./assets/images/icon-remove-item.svg"
+            alt="remove item"
+            class="cancel"
+          />
+        </div>
+      </div>
+      <hr class="order-divider" />
+      `
+      cartContainer.appendChild(cartItem)
+
+      const removeFromCart = cartItem.querySelector(
+        `[data-remove-from-cart="${item.id}"]`
+      )
+      removeFromCart.addEventListener('click', () => {
+        cartArray = cartArray.filter((cartItem) => cartItem.id !== item.id)
+        getCartItems()
+      })
+    })
+  }
+}
